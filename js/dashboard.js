1a8a0ff5bcdf15b8bec53575cdfa2833ce5fcaea
oscar-Rol5 <oscar.rol.work@gmail.com>
2024-11-23 17:22:31 +0100

    add new font, add download cv button, style contact me page

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,7 +8,7 @@ let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 4000;
+let timeRunning = 1000;
 let timeAutoNext = 10000;
 
 nextDom.onclick = function(){
